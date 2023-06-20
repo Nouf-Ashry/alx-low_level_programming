5552b01f51dd1daefcbb97d18212caefe91a3ad6
Nouf-Ashry <nono.ashry@gmail.com>
2023-06-20 11:14:05 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,4 @@
-#ifndef main_h
-
-#define main_h
-
-int _putchar(char);
+int _putchar(char c);
 
 void print_alphabet(void);
 
@@ -27,5 +23,3 @@ int add(int, int);
 void print_to_98(int n);
 
 void print_times_table(int n);
-
-#endif
